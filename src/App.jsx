389916fdd18fb265e2b9cca08081e951d4505902
arifa-iqbal <arifaiqbal1996@gmail.com>
2025-01-11 01:02:30 +0500

    import all three components in app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Title from "./Components/Title"
+import ClockSlogan from './Components/ClockSlogan'
+import ClockTiming from './Components/ClockTiming'
 
 const App = () => {
   return (
     <>
       <Title />
+      <ClockSlogan />
+      <ClockTiming />
     </>
   )
 }
